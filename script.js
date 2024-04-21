262a489fcb10c4b0d9e850d1f97a7cb88efccc2d
KhushiSahu2612 <143524034+KhushiSahu2612@users.noreply.github.com>
2024-04-21 12:01:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let a=[1,2,3,4,5,6,89,89,45,68];
+
+// a.forEach(function(val){
+//     console.log(val);
+//    // console.log(val+2);
+// })
+// let a=[1,2,3,4,5];
+// a.forEach(function(val){
+//     console.log(val+2);
+// });
+
+// let obj= {
+//     name:"khushi",
+//     Age:20,
+//     city:"Berla",
+// }
+// for (let key in obj) {
+//     console.log(key , obj[key]);
+//     //console.log(key);
+//     //console.log(obj[key]);
+        
+// }
+
+// let a=120;
+
+// do{
+//     console.log("hey");
+//     a++;
+// }
+// while(a<15)
+
+// setTimeout(function(){
+//     console.log("2 second Time Run")
+// },10000);
+
+let = {
+    name:"Khushi",
+    age:20,
+}
+delete a.age;
